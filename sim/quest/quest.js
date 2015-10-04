d9561cc18151318ffb2a6f0009d47cd7dd2a33ef
DR <daniel3735928559@gmail.com>
2015-10-03 22:21:53 -0500

    homework infrastructure updates

--- sim/quest/quest.js
+++ sim/quest/quest.js
@@ -8,7 +8,6 @@ app.controller("QuestController", ['$scope','$http','$window', '$timeout', '$doc
     $scope.wf_timeout = 0;
     console.log("AAAAAA",$scope.a);
     $scope.quests = []
-    $scope.quest_id = 0;
     $scope.quest = null;
     $scope.problems = []
     $scope.userdata = {};
@@ -16,14 +15,16 @@ app.controller("QuestController", ['$scope','$http','$window', '$timeout', '$doc
     $scope.p = {"type":"python"};
     $scope.userdata.current_problem = 0;
     $scope.set_problem = function(x){
+	console.log(x);
 	if($scope.userdata.current_problem == x) return;
-	if($scope.quest.problems[$scope.userdata.current_problem].type == "python"){
+	var ptype = $scope.quest.problems[$scope.userdata.current_problem].type
+	if(ptype == "python" || ptype == "advpython"){
 	    console.log("PPP",$scope.pycontrol.get_program(),$scope.quest.programs[$scope.userdata.current_problem]);
 	    $scope.quest.programs[$scope.userdata.current_problem] = $scope.pycontrol.get_program();
 	    $scope.userdata.current_problem = x;
 	    $scope.pycontrol.set_program($scope.quest.programs[$scope.userdata.current_problem]);
 	}
-	else if($scope.quest.problems[$scope.userdata.current_problem].type == "avr"){
+	else if(ptype == "avr"){
 	    console.log("PPP",$scope.avrcontrol.get_program(),$scope.quest.programs[$scope.userdata.current_problem]);
 	    $scope.quest.programs[$scope.userdata.current_problem] = $scope.avrcontrol.get_program();
 	    $scope.userdata.current_problem = x;
@@ -75,7 +76,8 @@ app.controller("QuestController", ['$scope','$http','$window', '$timeout', '$doc
 		}
 		$scope.quests.push(new_quest);
 		console.log("QS",$scope.quests);
-		$scope.quest = $scope.quests[0];
+		$scope.quest = new_quest;
+		console.log("QID",$scope.quest.id);
 		$scope.$emit('set_hwid',$scope.quest.id);
 		console.log("sending it down");
 		$scope.update_sim();
@@ -86,7 +88,7 @@ app.controller("QuestController", ['$scope','$http','$window', '$timeout', '$doc
     }
     $scope.update_sim = function(){
 	console.log($scope.pycontrol);
-	if($scope.pycontrol.set_program && $scope.quest && $scope.quest.problems[$scope.userdata.current_problem].type == "python")
+	if($scope.pycontrol.set_program && $scope.quest && ($scope.quest.problems[$scope.userdata.current_problem].type == "python" || $scope.quest.problems[$scope.userdata.current_problem].type == "advpython"))
 	    $scope.pycontrol.set_program($scope.quest.programs[$scope.userdata.current_problem]);
 	if($scope.avrcontrol.set_program && $scope.quest && $scope.quest.problems[$scope.userdata.current_problem].type == "avr")
 	    $scope.avrcontrol.set_program($scope.quest.programs[$scope.userdata.current_problem]);
@@ -103,17 +105,9 @@ app.controller("QuestController", ['$scope','$http','$window', '$timeout', '$doc
 	$scope.update_sim();
     });
     $scope.$on('jsavr_linked',function(event, data){
-	console.log("LALALALAA",data);
+	console.log("LALALALAAJ",data);
 	$scope.update_sim();
     });
-    $scope.$on('change_quest',function(event, data){
-	console.log("CHANGE",data);
-	$scope.update_sim();
-    });
-    $scope.$on('get_program_data',function(event, data){
-	$scope.$emit('program_data',{'problems':$scope.quest.problems,'programs':$scope.quest.programs});
-    });
-//    $timeout($scope.getstuff, $scope.wf_timeout);
     console.log($scope.problems);
 }])
     .directive('exploration',function(){
@@ -130,6 +124,16 @@ app.controller("QuestController", ['$scope','$http','$window', '$timeout', '$doc
 		scope.quest_name = attrs.questName;
 		console.log("LINKY",scope,element,attrs);
 		scope.getstuff(scope.quest_name);
+		if(scope.control){
+		    scope.control.set_quest = function(qid,qname){
+			console.log("qid",qid,scope.quest_name);
+			scope.hwid = qid;
+			scope.getstuff(qname);
+		    }
+		    scope.control.get_program_data = function(){
+			return {'problems':scope.quest.problems,'programs':scope.quest.programs}
+		    }
+		}
 	    }
 	}
     });
